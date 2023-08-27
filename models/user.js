fc860d5b312a79e7f0a8311128f3d0015110a084
ndaly94 <ndaly94@gmail.com>
2023-08-27 12:59:18 -0400

    user model added

---
+++ models/user.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
+
+const SALT_ROUNDS = 6;
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    email: {
+        type: String,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        required: true
+    },
+    password: {
+        type: String,
+        trim: true,
+        minlength: 3,
+        required: true
+    }
+},  {
+    timestamps: true,
+    toJSON: {
+        transform: function(doc, ret) {
+            delete ret.password;
+            return ret;
+        }
+    },
+    //favorties array
+    favorites: {
+        type: Array,
+        item: itemSchema,
+    },
+    //shop_id or null turnary
+    shop: {
+        type: Boolean,
+        shop: shopSchema
+    }
+});
+
+userSchema.pre('save', async function(next) {
+    // 'this' is the use document
+    if (!this.isModified('password')) return next();
+    // update password with computed hash
+    this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+    return next();
+})
+
+module.exports = mongoose.model('User', userSchema);
