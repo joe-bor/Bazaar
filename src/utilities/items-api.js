106e6d85d6d6772006b02ecb4766f2b4744ec374
joe-bor <kimborlongan_08@yahoo.com>
2023-08-27 15:53:24 -0700

    users-api | item-api | shops-api | orders-api | reviews-api

---
+++ src/utilities/items-api.js
@@ -0,0 +1,39 @@
+import sendRequest from './send-request';
+
+const BASE_URL = '/api/items';
+
+export function createItem(itemInfo) {
+    return sendRequest(BASE_URL, 'POST', itemInfo)
+}
+
+export function getAll() {
+	return sendRequest(BASE_URL);
+}
+
+export function getById(id) {
+	return sendRequest(`${BASE_URL}/${id}`);
+}
+
+export function editItemInfo(id, newInfo) {
+    return sendRequest(`${BASE_URL}/${id}`, 'PUT', newInfo)
+}
+
+export function deleteItem(id) {
+    return sendRequest(`${BASE_URL}/${id}`, 'DELETE')
+}
+
+export function getItemReviews(id) {
+    return sendRequest(`${BASE_URL}/reviews/${id}`)
+}
+
+/*
+
+itemShape = {
+    Name 
+    Price
+    Comments: comments array
+    Description
+    Category - used for displaying ‘similar items’
+}
+
+ */
