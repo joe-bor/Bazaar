b47c7b5d5c1f62abe137c1ce8ec61cd29bff5f1d
Anmitchell <avarynmitchell@gmail.com>
2023-08-28 14:13:45 -0400

    Added added reviews routes

--- routes/api/reviews.js
+++ routes/api/reviews.js
@@ -8,10 +8,10 @@ router.get('/:itemId', reviewsCtrl.index)
 // POST -> /api/reviews - create a review
 router.post('/', reviewsCtrl.create)
 
-// PUT -> /api/items - update a review
+// PUT -> /api/reviews/:id - update a review
 router.put('/:id', reviewsCtrl.update)
 
-// DELETE -> /api/items/:id - delete a review
+// DELETE -> /api/reviews/:id - delete a review
 router.delete('/:id', reviewsCtrl.delete)
 
 module.exports = router
