36fc47723db4c3801ad3ce23d4921c9d91be3bb2
joe-bor <kimborlongan_08@yahoo.com>
2023-08-30 13:45:40 -0700

    Shop model: change products property

--- models/shop.js
+++ models/shop.js
@@ -5,7 +5,7 @@ const shopSchema = new mongoose.Schema({
     seller: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     heroImage: { type: String, required: true },
     rating: { type: Number, default: 0 },
-    products: [{ type: mongoose.Schema.Types.ObjectId, ref: ' User', required: true }]
+    products: [{ type: mongoose.Schema.Types.ObjectId, ref: ' User' }]
 })
 
 module.exports = mongoose.module('Shop', shopSchema)
