cc3d01cb01053332755c38980ce3cae8ea2483ca
hlysllrs <hailey.sellars@gmail.com>
2023-08-31 16:54:42 -0700

    updated order routes to allow ordering from multiple shops at once

--- models/itemSchema.js
+++ models/itemSchema.js
@@ -1,39 +1,45 @@
 const Schema = require('mongoose').Schema
 
-const itemSchema = new Schema({ 
-    name: { 
-        required: true, 
-        type: String, 
-        set: function(value){
+const itemSchema = new Schema({
+    name: {
+        required: true,
+        type: String,
+        set: function (value) {
             let split = value.split(' ')
             if (split.length === 1) return value.charAt(0).toUpperCase() + value.slice(1)
 
-            split.forEach((string, index, array) => { array[index] = string.charAt(0).toUpperCase() + string.slice(1)})
+            split.forEach((string, index, array) => { array[index] = string.charAt(0).toUpperCase() + string.slice(1) })
             let toString = split.toString().replaceAll(',', ' ')
             return toString
-        } 
+        }
     },
-    price: { 
-        required: true, 
-        type: Number, 
-        set: function(value){
-            return Math.round(value * 100) / 100 
-        } 
+    price: {
+        required: true,
+        type: Number,
+        set: function (value) {
+            return Math.round(value * 100) / 100
+        }
     },
-    reviews:[{ 
-        type: Schema.Types.ObjectId, 
-        ref:'Review' 
+    reviews: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Review'
     }],
-    description: { 
-        required: true, 
-        type: String 
+    description: {
+        required: true,
+        type: String
     },
-    category: { 
-        required: true, 
-        type: Schema.Types.ObjectId, 
-        ref:'Category' }
+    category: {
+        required: true,
+        type: Schema.Types.ObjectId,
+        ref: 'Category'
+    },
+    shop: {
+        required: true,
+        type: Schema.Types.ObjectId,
+        ref: 'Shop'
+    }
 }, {
     timestamps: true
-}) 
+})
 
 module.exports = itemSchema
--- models/shop.js
+++ models/shop.js
@@ -5,7 +5,7 @@ const shopSchema = new mongoose.Schema({
     seller: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     heroImage: { type: String, required: true },
     rating: { type: Number, default: 0 },
-    products: [{ type: mongoose.Schema.Types.ObjectId, ref: ' Item' }]
+    products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item' }]
 })
 
 module.exports = mongoose.model('Shop', shopSchema)
