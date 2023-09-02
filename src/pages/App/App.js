84bc1ffbb1b8eb47c58308236b57d38960c2bfc5
Brendan Mohan <bmohan2795@gmail.com>
2023-09-01 19:16:17 -0500

    completion of the App.js file. Creation of guest functionality (with help from Haily). Importation and route paths are accurate from what I can tell. Might have misused location and set user within the paths as an FYI. Will reconvene with everyone about this in next weeks meeting.

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,9 +1,90 @@
+import React, { useState, useEffect } from 'react';
+import { Routes, Route, Navigate, useNavigate, useLocation } from 'react-router-dom';
+import styles from './App.module.scss';
+import Home from '../Users/Home';
+import Main from '../Users/Main';
+import ItemDetails from '../Users/ItemDetails';
+import Favorites from '../Users/Favorties';
+import Cart from '../Users/Cart';
+import Checkout from '../Users/Checkout';
+import OrderHistory from '../Users/OrderHistory';
+import Account from '../Users/AccountPage';
+// Wireframe calls it User Profile Page, VSCode file calls it AccountPage here's the alternative
+// import UserProfile from '../Users/UserProfile;
+import ShopMgmt from '../Sellers/ShopManagement';
+import NavBar from '../../components/navbar/navbar';
+import { getUser, signUp } from '../../utilities/users-service';
+
+export default function App() {
+  const [user, setUser] = useState(getUser())
+  const [cart, setCart] = useState(null)
+  const navigate = useNavigate()
+  let location = useLocation()
 
 
-function App() {
+  // auto log-in as guest user
+  useEffect(() => {
+    if (!user) {
+      createGuestUser()
+    }
+  }, [])
+
+  async function createGuestUser() {
+    const guestUserData = {
+      // random email generated
+      email: Math.round(Math.random() * 100000000) + '@guest',
+      // assign guest name
+      name: 'c186ec',
+      // set guest password
+      password: 'guestpass'
+    }
+    localStorage.setItem('guest', guestUserData.email)
+    const guestUser = await signUp(guestUserData)
+    // set user to newly created guest user
+    setUser(guestUser)
+  }
+
+  // clicking on logo takes you home
+  function handleLogoClick() {
+    navigate('/home')
+  }
+
+  // <Logo onClick={handleLogoClick}></Logo>
+
   return (
-    <div>App Page</div>
+    <main className={styles.App}>
+      <Logo className={styles.Logo} location={location} handleLogoClick={handleLogoClick} />
+      <NavBar 
+        className={styles.NavBar}
+        user={user}
+        cart={cart}
+        location={location} />
+      <Routes>
+        {/* client-side route that renders the component instance if the patch matches the url in the address bar */}
+        <Route path="/home" element={<Home className={styles.Home} setCart={setCart} />} />
+        <Route path="/main" element={<Main className={styles.Main} />} />
+        <Route path="/itemdetails" element={<ItemDetails />} />
+        <Route path="/account" element={<Account className={styles.AccountPage} user={user} setUser={setUser} location={location}  />} />
+        <Route path="/favorites" element={<Favorites /> } />
+        <Route path="/cart" element={<Cart className={styles.Cart} />} />
+        <Route path="/checkout" element={<Checkout className={styles.Checkout} />} />
+        <Route path="/orderhistory" element={<OrderHistory user={user} setUser={setUser} location={location} />} />
+        <Route path="/sellershop" element={<SellerShop user={user} setUser={setUser} />} />
+        <Route path="/shopmgmt" element={<ShopMgmt user={user} setUser={setUser} />} />
+        {/* redirect to /home if path in address bar hasn't matched a <Route> above */}
+        <Route path="/*" element={<Navigate to="/home" />} />
+      </Routes>
+    </main>
   )
 }
 
-export default App
+
+
+
+// function App() {
+//   return (
+//     <div>App Page</div>
+//   )
+// }
+
+// export default App
