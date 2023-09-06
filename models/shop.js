9b898f1e0c80642a963b15750b1f8d6ce6798362
hlysllrs <hailey.sellars@gmail.com>
2023-09-06 09:15:58 -0700

    progress on shop and product components

--- models/shop.js
+++ models/shop.js
@@ -4,6 +4,7 @@ const shopSchema = new mongoose.Schema({
     name: { type: String, required: true },
     seller: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     heroImage: { type: String, required: true },
+    description: { type: String, required: true },
     rating: { type: Number, default: 0 },
     products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item' }]
 })
