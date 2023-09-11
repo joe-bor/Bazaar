e8e036614f31e306e7f7b97b8ed2c63a1c5614f4
hlysllrs <hailey.sellars@gmail.com>
2023-09-10 23:27:10 -0400

    implement create a shop form and start of shop management page

--- src/components/createShop/createShop.js
+++ src/components/createShop/createShop.js
@@ -1,58 +1,87 @@
 import { useState } from 'react'
-import { createShop } from '../../utilities/shops-api'
-// import update user function from user-service to update user's shop property
+import { createShop, editShopInfo } from '../../utilities/shops-api'
+import styles from './CreateShop.module.scss'
+import FormInput from '../FormInput/FormInput'
+import { useNavigate } from 'react-router-dom'
 
-export default function CreateShop({ }) {
-  const [shopData, setShopData] = useState({
+export default function CreateShop({ user, setUser, location, shop, setShop }) {
+  const [shopValues, setShopValues] = useState({
     name: '',
     description: '',
-    logoImage: null // 🟥 add shop logo to shop schema 🟥
+    // logoImage: null
   })
-  const [error, setError] = useState('')
-  const [showShopModal, setShowShopModal] = useState(false)
+  const navigate = useNavigate()
 
-  function toggleShopModal() {
-    setShowShopModal(!showShopModal)
+
+  const shopInputs = [
+    {
+      id: "shop-name",
+      name: "name",
+      type: "text",
+      placeholder: "Shop Name",
+      value: location.pathname === './account' ? shopValues.name : shop?.name,
+      errorMessage:
+        "Shop name is required and can't include special characters",
+      label: "Shop Name",
+      pattern: "^[A-Za-z0-9 ']+$",
+      required: true,
+    },
+    {
+      id: "shop-description",
+      name: "description",
+      type: "text",
+      placeholder: "Shop Description",
+      value: location.pathname === './account' ? shopValues.description : shop?.description,
+      errorMessage: "Shop description is required",
+      label: "Shop Description",
+      required: true,
+    }
+  ]
+
+  const logoInputProps = {
+    id: "shop-logo",
+    name: "file",
+    type: "file",
+    accept: ".png, .jpg, .jpeg",
+    errorMessage:
+      "File type must be .png, .jpeg, or .jpg",
+    label: "Shop Logo Image"
   }
 
-  function handleChange(e) {
-    setShopData({ ...shopData, [e.target.name]: e.target.value })
-    setError('')
+  const handleShopInputChange = (e) => {
+    setShopValues({ ...shopValues, [e.target.name]: e.target.value })
   }
 
   // 🟥 function for handling image upload 🟥
 
-  function handleSubmit(e) {
+  async function handleShopSubmit(e) {
     e.preventDefault()
-    try {
-      const formData = { ...shopData }
+    const formData = { ...shopValues }
+    if (location.pathname === '/shopmgmt') {
+      // send request to update shop
+      const updatedShop = await editShopInfo(formData)
+      setShop(updatedShop)
+
+    } else {
       // send request to create shop
-      createShop(formData)
-      // 🟥 update user's shop property with shop._id 🟥
-      // 🟥 set user state to have updated user info 🟥
-      toggleShopModal()
-    } catch {
-      // an error happened on the server
-      setError('shop creation failed - try again')
+      const newData = await createShop(formData)
+      // set user state to have shop info
+      console.log(newData.user)
+      setUser(newData.user)
+      // navigate('/shopmgmt')
     }
   }
 
   return (
     <div className={styles.CreateShop}>
-      <div className={styles.closeBtn} onClick={toggleShopModal}>X</div>
       <div className="form-container">
-        <form autoComplete="off" onSubmit={handleSubmit}>
-          <label>Shop Name</label>
-          <input type="text" name="name" value={shopData.name} onChange={handleChange} required />
-          <label>Shop Description</label>
-          <input type="text" name="description" value={shopData.description} onChange={handleChange} required />
-          {/* 🟥 need to be able to upload images with express-fileupload and cloudinary 🟥 */}
-          <label>Shop Logo Image</label>
-          <input type="file" name="logoImage" accept=".png, .jpg, .jpeg" value={shopData.logoImage} onChange={handleChange} />
-          <button type="submit">Create Shop</button>
+        <h1 className={styles.h1}>{location.pathname === './account' ? 'Create A Shop' : 'Edit Shop Details'}</h1>
+        <form autoComplete="off" onSubmit={handleShopSubmit}>
+          {/* <FormInput {...logoInputProps} handleInputChange={handleImageChange} /> */}
+          {shopInputs.map(input => <FormInput key={input.id} {...input} value={shopValues[input.name]} handleInputChange={handleShopInputChange} />)}
+          <button formMethod='dialog'>{location.pathname === '/account' ? 'Create Shop' : 'Update Shop'}</button>
         </form>
       </div>
-      <p className="error-message">{error}</p>
     </div>
   )
 }
--- src/pages/AccountPage/AccountPage.js
+++ src/pages/AccountPage/AccountPage.js
@@ -2,21 +2,33 @@ import UserLogOut from "../../components/UserLogOut/UserLogOut";
 import EditUserForm from "../../components/EditUserForm/EditUserForm";
 import userIcon from '../../assets/images/user-icon.svg'
 import { useState, useRef, useEffect } from 'react'
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useLocation, useNavigate } from "react-router-dom"
 import { deleteUser } from "../../utilities/users-service";
 import styles from './AccountPage.module.scss'
+import CreateShop from "../../components/CreateShop/CreateShop";
 
 export default function AccountPage({ user, setUser, createGuestUser }) {
   const [editModalOpen, setEditModalOpen] = useState(false)
+  const [shopModalOpen, setShopModalOpen] = useState(false)
   const editModalRef = useRef()
+  const shopModalRef = useRef()
   const navigate = useNavigate()
+  const location = useLocation()
 
   useEffect(() => {
     editModalOpen ? editModalRef.current.showModal() : editModalRef.current.close()
   }, [editModalOpen])
 
+  useEffect(() => {
+    shopModalOpen ? shopModalRef.current.showModal() : shopModalRef.current.close()
+  }, [shopModalOpen])
+
   function toggleEditModal() {
     setEditModalOpen(!editModalOpen)
+  }
+
+  function toggleCreateShop() {
+    setShopModalOpen(!shopModalOpen)
   }
 
   async function deleteUserAcc() {
@@ -28,24 +40,26 @@ export default function AccountPage({ user, setUser, createGuestUser }) {
   return (
     <div>
       <div className={styles.userImage}>
-        <img src={user.name === 'c186ec' ? userIcon : user.imageUrl} />
+        <img src={user.name === 'c186ec' || !user.imageUrl ? userIcon : user.imageUrl} />
       </div>
       <div className={styles.userInfo}>
         <h1>{user.name}</h1>
-        <p>{user.email}</p>
-        <p>**********</p>
+        <p>{`Email: ${user.email}`}</p>
+        <p>Password: ********</p>
       </div>
       <div className={styles.links}>
         <button onClick={toggleEditModal}>Edit Profile</button>
         <Link to='/orderhistory'><p>Order History</p></Link>
-        <Link to='/shopmgmt'><p>Manage Shop</p></Link>
+        {user.shop ? <Link to='/shopmgmt'><p>Manage Shop</p></Link> : <button onClick={toggleCreateShop}>Create a Shop</button>}
         <button onClick={deleteUserAcc}>Delete Account</button>
         <UserLogOut createGuestUser={createGuestUser} />
       </div>
       <div>
+        <h2>Favorites</h2><button onClick={() => navigate('favorites')}>See All</button>
         {/* 🟥 Favorites Section Here 🟥 */}
       </div>
       <dialog ref={editModalRef} onClose={toggleEditModal}><EditUserForm user={user} setUser={setUser} /></dialog>
+      <dialog ref={shopModalRef} onClose={toggleCreateShop}><CreateShop user={user} setUser={setUser} location={location} /></dialog>
     </div>
   )
 }
